285abc2be09a059b48a556221e5e88c1e03b1087
abhinand0d <abhinand0d@gmail.com>
2021-05-26 16:25:23 +0530

    Build 1

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 This is telegram bot that uploads cat image every 10 minutes. The bot is made for cat lovers who like to see pictures of cats.
 
+### How to deploy my bot??
+- secrets to be filled by you
+SESSION_STRING - Generate Session String and add to heroku or secrets
+API_ID - Go to https://my.telegram.org for getting it and add it to secrets
+API_HASH - Go to https://my.telegram.org for getting it and add it to secrets
+BOT_TOKEN - Create it with help of Bot Father
+LOGGING_CHANNEL - Give your bot permission in Logging channel and get ID of the channel begins with -
 ### Thank You Note:
 - [Developer of Cat as a Service](https://cataas.com/#/)
+- [Pyrogram] (https://github.com/pyrogram)
---
+++ main.py
@@ -0,0 +1,68 @@
+from logging import getLoggerClass
+from pyrogram import Client,filters
+import os
+import time
+import urllib.request
+
+app = Client(session_name=os.environ['SESSION_STRING'],api_id=os.environ['API_ID'],api_hash=os.environ['API_HASH'],bot_token=os.environ['BOT_TOKEN'])
+
+
+#  Creating of a Image Folder
+if os.path.exists("IMG"):
+    print("Path Exists")
+else:
+    os.makedirs("IMG")
+
+chitchat_users = []
+
+def uploader():
+    print("Im Sending Photo...")
+    urllib.request.urlretrieve(url="https://cataas.com/cat",filename="IMG/cat.jpg")
+    for user in chitchat_users:
+        app.send_photo(chat_id=user,photo="IMG/cat.jpg")
+        print(user)
+    print(chitchat_users)
+
+def cat_imager():
+    global send_pics
+    print("doing This")
+    while int(send_pics) == 1:
+        if chitchat_users == []:
+            print("No User To Send Photo") 
+            break
+        else:    
+            uploader()
+
+        time.sleep(600)
+        
+
+
+
+@app.on_message(filters.command("start"))
+def start_command(client,message):
+    global chitchat,user_name,send_pics
+    chitchat = message['chat']['id']
+    user_name = message['chat']['username']
+    print("\nHey Welcome use ",user_name)
+    print("\nHaving id of ",chitchat)
+    try:
+        app.send_message(chat_id=os.environ['LOGGING_CHANNEL'],text="We got a new user "+ str(chitchat))
+    except:
+        print("Please add your channel to log informations")
+    send_pics = 1
+    chitchat_users.append(chitchat)
+    app.send_message(chat_id=chitchat,text="Hey "+user_name+" You have started the Cat Image Bot will sent you cat Image from now every 10 mins.")
+    cat_imager()
+
+@app.on_message(filters.command("stop"))
+def stop_command(client,message):
+    global stchitchat,send_pics
+    stchitchat = message['chat']['id']
+    app.send_message(chat_id=stchitchat,text="Thank You for Using our service...")
+    try:
+        app.send_message(chat_id=os.environ['LOGGING_CHANNEL'],text="We lost a user "+ str(chitchat))
+    except:
+        print("Please add your channel to log informations")
+    chitchat_users.remove(stchitchat)
+
+app.run()
